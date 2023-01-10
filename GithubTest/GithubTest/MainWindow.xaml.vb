f8710854099481ca310d5ee8490d93f93497088a
Alan Wang <alanwang@vt.edu>
2023-01-10 14:27:00 -0500

    hw1

---
+++ GithubTest/GithubTest/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Class MainWindow
+    Private Sub OkButton_Click(sender As Object, e As RoutedEventArgs) Handles OkButton.Click
+        Dim languageInput As String = LanguageTextbox.Text.ToLower()
+
+        If languageInput = "vb.net" Then
+            MsgBox("good choice")
+        End If
+
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As RoutedEventArgs) Handles CancelButton.Click
+        Me.Close()
+    End Sub
+
+End Class
